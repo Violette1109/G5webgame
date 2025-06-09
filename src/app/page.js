8cb4af814172eaedb8575b5f5cbd610ee98eed9c
aorcnoux <april.chou92@gmail.com>
2025-06-10 01:58:21 +0800

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -78,7 +78,7 @@ export default function Home() {
 
       {/* 迷宮遊戲圈圈 */}
       <div className="group absolute top-[52%] left-[20%] w-[12%] h-[20%] rounded-full bg-white/0 hover:bg-white/10 transition cursor-pointer flex items-center justify-center">
-        <Link href="">
+        <Link href="https://my-dessert-maze.vercel.app/">
           <Image
             src={StartImg}
             alt="start"
