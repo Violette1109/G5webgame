59f35737fc62504e22f4fce4b021eb1c18a8997b
yucenliao <yucenliao0411@gmail.com>
2025-06-11 00:21:34 +0800

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -89,7 +89,7 @@ export default function Home() {
 
       {/* 翻牌遊戲圈圈 */}
       <div className="group absolute top-[76%] left-[68%] w-[12%] h-[20%] rounded-full bg-white/0 hover:bg-white/10 transition cursor-pointer flex items-center justify-center">
-        <Link href="">
+        <Link href="https://dessert-rho.vercel.app/">
           <Image
             src={StartImg}
             alt="start"
